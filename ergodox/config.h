1fc4cdffa7a5a6bf46817fc9c61aab0a2eb3ceba
Bernardo Farah <ber@bernardo.me>
2017-10-24 19:01:53 -0700

    ergodox: Created my own custom keyboard layout

---
+++ ergodox/config.h
@@ -0,0 +1,18 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+#endif
+
+#include "../../config.h"
+
+#ifdef NO_DEBUG
+  #undef NO_DEBUG
+#endif
+
+#ifdef IGNORE_MOD_TAP_INTERRUPT
+  #undef IGNORE_MOD_TAP_INTERRUPT
+#endif
+/* #define FORCE_NKRO */
+/* #define PREVENT_STUCK_MODIFIERS */
+
+/* #undef TAPPING_TERM */
+/* #undef IGNORE_MOD_TAP_INTERRUPT */
---
+++ ergodox/readme.md
@@ -0,0 +1,4 @@
+# Bernardo's modified version of the OSX defaults
+
+All the commands!
+Also, I map F19 to spotlight, I like having that handy
