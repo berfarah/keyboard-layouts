96cfa80c44642f08c60edd62bb69489f4ac05deb
Bernardo Farah <bernardo.farah@samsara.com>
2022-09-19 21:08:56 -0700

    lily58: Updated to latest QMK configuration

--- lily58/keymap.c
+++ lily58/keymap.c
@@ -132,7 +132,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 };
 
+#ifdef RGBLIGHT_ENABLE
 int RGB_current_mode;
+#endif
 
 bool is_windows_mode(void) {
   return IS_LAYER_ON(_WIN);
@@ -153,7 +155,18 @@ void matrix_init_user(void) {
     #endif
 }
 
-#ifdef OLED_DRIVER_ENABLE
+enum rotary_mode {
+  VOLUME = 0,
+  BRIGHTNESS,
+  LED_BRIGHTNESS,
+  LED_HUE,
+  LED_SATURATION,
+  LED_MODE,
+};
+int CURRENT_ROTARY_MODE = 0;
+
+
+#ifdef OLED_ENABLE
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
   /* if (is_keyboard_master()) { */
@@ -192,21 +205,11 @@ const char *read_custom_layer_state(void) {
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: VIMDow");
     break;
   default:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%ld", layer_state);
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef");
   }
 
   return layer_state_str;
 }
-
-enum rotary_mode {
-  VOLUME = 0,
-  BRIGHTNESS,
-  LED_BRIGHTNESS,
-  LED_HUE,
-  LED_SATURATION,
-  LED_MODE,
-};
-int CURRENT_ROTARY_MODE = 0;
 
 
 // When add source files to SRC in rules.mk, you can use functions.
@@ -231,22 +234,30 @@ const char *read_rotary(void) {
       snprintf(rotary_message, sizeof(rotary_message), "BRIGHTNESS");
       break;
     case LED_BRIGHTNESS:
+      #ifdef RGBLIGHT_ENABLE
       snprintf(rotary_message, sizeof(rotary_message), "LED: VAL  - %d%%", rgblight_config.val*100/255);
+      #endif
       break;
     case LED_HUE:
+      #ifdef RGBLIGHT_ENABLE
       snprintf(rotary_message, sizeof(rotary_message), "LED: HUE  - %d", rgblight_config.hue);
+      #endif
       break;
     case LED_SATURATION:
+      #ifdef RGBLIGHT_ENABLE
       snprintf(rotary_message, sizeof(rotary_message), "LED: SAT  - %d%%", rgblight_config.sat*100/255);
+      #endif
       break;
     case LED_MODE:
+      #ifdef RGBLIGHT_ENABLE
       snprintf(rotary_message, sizeof(rotary_message), "LED: MODE - %d", rgblight_config.mode);
+      #endif
       break;
   }
   return rotary_message;
 }
 
-void oled_task_user(void) {
+bool oled_task_user(void) {
   if (is_keyboard_master()) {
     // If you want to change the display of OLED, you need to change here
     oled_write_ln(read_custom_layer_state(), false);
@@ -259,12 +270,13 @@ void oled_task_user(void) {
   } else {
     oled_write(read_logo(), false);
   }
+  return false;
 }
 
 #endif // OLED Driver
 
 #ifdef ENCODER_ENABLE
-void encoder_update_user(uint8_t index, bool clockwise) {
+bool encoder_update_user(uint8_t index, bool clockwise) {
   if (index == 0) {
     switch(CURRENT_ROTARY_MODE) {
       case VOLUME:
@@ -282,41 +294,50 @@ void encoder_update_user(uint8_t index, bool clockwise) {
         }
         break;
       case LED_BRIGHTNESS:
+      #ifdef RGBLIGHT_ENABLE
         if (clockwise) {
           rgblight_decrease_val();
         } else {
           rgblight_increase_val();
         }
+        #endif
         break;
       case LED_HUE:
+      #ifdef RGBLIGHT_ENABLE
         if (clockwise) {
           rgblight_decrease_hue();
         } else {
           rgblight_increase_hue();
         }
+        #endif
         break;
       case LED_SATURATION:
+      #ifdef RGBLIGHT_ENABLE
         if (clockwise) {
           rgblight_decrease_sat();
         } else {
           rgblight_increase_sat();
         }
+        #endif
         break;
       case LED_MODE:
+      #ifdef RGBLIGHT_ENABLE
         if (clockwise) {
           rgblight_step_reverse();
         } else {
           rgblight_step();
         }
+        #endif
         break;
     }
   }
+  return true;
 }
 #endif
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
-#ifdef OLED_DRIVER_ENABLE
+#ifdef OLED_ENABLE
     set_keylog(keycode, record);
 #endif
     // set_timelog();
